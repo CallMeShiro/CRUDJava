4bd3075b385d13b8b47286af2612e8cec03dd183
CallMeShiro <jimkarhulinna@hotmail.com>
2020-01-28 12:12:48 +0100

    Update DataAccess.java

--- src/java/dao/DataAccess.java
+++ src/java/dao/DataAccess.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package dao;
 
 import db.DBUtils;
@@ -16,10 +10,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.News;
 
-/**
- *
- * @author Thang
- */
 public class DataAccess {
     public void addNew(News n){
         try {
